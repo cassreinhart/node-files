70f8be466b2ee501b04116eaa6004c3e2af27d36
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2023-01-10 11:55:14 -0600

    step3

--- step3.js
+++ step3.js
@@ -2,29 +2,51 @@ import fs from "fs";
 import axios from 'axios';
 import process from "process";
 
-function cat(path) {
+function handleOutput(data, out) {
+    if (out) {
+        fs.writeFile(out, data, 'utf8', function(err) {
+            if (err) {
+                console.error(`Error writing ${out}: ${err}.`)
+                process.exit(1)
+            }
+        });
+    } else {
+        console.log(data)
+    }
+}
+
+function cat(path, out) {
 
     fs.readFile(path, 'utf8', function(err, data) {
         if (err) {
-            console.error('ERROR:', err)
-            process.exit(1)
+            console.error('ERROR:', err);
+            process.exit(1);
+        } else {
+            handleOutput(data, out);
         }
-        console.log(data);
     });
 }
 
-async function webCat(url) {
+async function webCat(url, out) {
 
     try {
         let webPage = await axios.get(url)
-        console.log(webPage.data);
+        handleOutput(webPage.data, out)
     } catch (err) {
         console.error(`Error: Cannot get ${url}: ${err}.`)
         process.exit(1)
     }
 }
 
-let path = process.argv[2];
+let out;
+let path;
+
+if (process.argv[2] === '--out') {
+    out = process.argv[3];
+    path = process.argv[4];
+} else {
+    path = process.argv[2];
+}
 
 if (path.slice(0, 4) === 'http') {
     webCat(path);
